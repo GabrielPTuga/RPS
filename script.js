0403494510e83d7f42b56cbf79ea89360003d71b
GabrielPTuga <ribeiroportugalgabriel@gmail.com>
2025-04-16 19:39:58 -0100

    Adicionando Css

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ if (pontuacao === null) {
     }
 }
 
+const ganperd = document.querySelector(".js-result");
 updateScoreElement();
 
+// Funcção para escolher a opção do jogador
 function pickComputerMove() {
     const randomNumber = Math.floor(Math.random()*10);
 
@@ -21,49 +23,46 @@ function pickComputerMove() {
         return "Tesoura";
     }
 }
-/*
-function playGame() {
-    let computer = pickComputerMove();
-    const player = prompt("Digite (Pedra, Papel ou Tesoura");
 
-    if((player == "Pedra" && computer == "Pedra") || (player =="Papel" && computer == "Papel") || (player == "Tesoura" && computer == "Tesoura")) {
-        console.log(`Player: ${player} vs Computer ${computer}`);
-        console.log("Empate");
-    } else if ((player == "Pedra" && computer=="Tesoura") || (player == "Papel" && computer=="Pedra") || (player =="Tesoura" && computer=="Papel")) {
-        console.log(`Player: ${player} vs Computer ${computer}`);
-        console.log("Player ganhou");
-    } else {
-        console.log(`Player: ${player} vs Computer ${computer}`);
-        console.log("Conputer ganhou");
-    }
-}
-*/
-
+// Funcção para iniciar o jogo
 function playGame2(escolha) {
     let computer = pickComputerMove();
     const player = escolha;
 
+
     if((player == "Pedra" && computer == "Pedra") || (player =="Papel" && computer == "Papel") || (player == "Tesoura" && computer == "Tesoura")) {
-        pontuacao.Empate += 1
-        document.querySelector(".js-result").innerHTML = "Empatou!"
+        pontuacao.Empate += 1;
+        ganperd.classList.add("js-empate");
+        ganperd.classList.remove("js-ganhou")
+        ganperd.classList.remove("js-perdeu")
+        ganperd.innerHTML = "Empatou!";
         playerComputer(player,computer);
     } else if ((player == "Pedra" && computer=="Tesoura") || (player == "Papel" && computer=="Pedra") || (player =="Tesoura" && computer=="Papel")) {
         pontuacao.Ganhou += 1;
-        document.querySelector(".js-result").innerHTML = "Ganhou!"
+        ganperd.innerHTML = "Ganhou!";
+        ganperd.classList.add("js-ganhou");
+        ganperd.classList.remove("js-empate")
+        ganperd.classList.remove("js-perdeu")
         playerComputer(player,computer);
         
     } else {
-        pontuacao.Perdeu += 1
-        document.querySelector(".js-result").innerHTML = "Perdeu!"
+        pontuacao.Perdeu += 1;
+        ganperd.innerHTML = "Perdeu!";
+        ganperd.classList.add("js-perdeu");
+        ganperd.classList.remove("js-ganhou")
+        ganperd.classList.remove("js-empate")
         playerComputer(player,computer);
         
     }
 
+    // Adicionar a função no localstorage
     localStorage.setItem("pontuacao", JSON.stringify(pontuacao));
 
+    // Atualizar os resultados
     updateScoreElement();
 }
 
+// Função para resetar a pontuação
 function resetarPontuacao() {
     pontuacao.Ganhou = 0;
     pontuacao.Empate = 0;
@@ -72,12 +71,16 @@ function resetarPontuacao() {
     updateScoreElement();
     document.querySelector(".js-moves").innerHTML = ` `
     document.querySelector(".js-result").innerHTML = "Pontuação Resetada!";
+    ganperd.classList.add("js-perdeu");
+    ganperd.classList.remove("js-empate");
 }
 
+// Função que vai atualizar a pontuação do jogo
 function updateScoreElement() {
     document.querySelector(".js-score").innerHTML = `Ganhou: ${pontuacao.Ganhou}, Empate: ${pontuacao.Empate}, Perdeu: ${pontuacao.Perdeu}`
 };
 
+// Função que vai Mostrar qual foi a opção que o jogador escolheu e qual que o computador escolheu
 function playerComputer(player,computer) {
     document.querySelector(".js-moves").innerHTML = `Player: ${player} vs Computer ${computer}`
 };
